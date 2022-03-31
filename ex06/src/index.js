9a5e4d9929c24962c1fbdf27b13db97d9700ed13
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-03-31 11:57:29 +0200

    Add Exercise 06

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo){
+   
+    // Only change code below this line
+    var myQuotient = numOne / numTwo;  
+    // Only change code above this line
+    
+   return myQuotient;
+
+}
+    
+console.log(main(102.0, 25.5));   
+module.exports = main;
